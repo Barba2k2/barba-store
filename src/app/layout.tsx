39c4ead362a2e5d807ff9cc385bdcc042a5e9181
Barba2k2 <lorenzo.dz@hotmail.com>
2024-02-14 22:25:26 -0300

    STYLE: Ensure footer is always in the botton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,12 +20,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          <Header />
-          {children}
+        <div className="flex h-full flex-col">
+          <AuthProvider>
+            <Header />
+            <div className="flex-1">{children}</div>
 
-          <Footer />
-        </AuthProvider>
+            <Footer />
+          </AuthProvider>
+        </div>
       </body>
     </html>
   );
